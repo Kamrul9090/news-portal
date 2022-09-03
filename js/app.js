a99493babe40d574a04bade2de68265a7b027dac
Kamrul Hasan <CR7.kamrul.9090@gmail.com>
2022-09-03 10:30:06 +0600

    news section complete

--- js/app.js
+++ js/app.js
@@ -1,8 +1,12 @@
 const loadCategories = async () => {
-    const url = `https://openapi.programming-hero.com/api/news/categories`;
-    const res = await fetch(url);
-    const catagory = await res.json();
-    displayCategories(catagory.data.news_category);
+    try {
+        const url = `https://openapi.programming-hero.com/api/news/categories`;
+        const res = await fetch(url);
+        const catagory = await res.json();
+        displayCategories(catagory.data.news_category);
+    } catch (error) {
+        console.log(error)
+    }
 }
 
 //Display categories items
@@ -44,13 +48,53 @@ const loadNews = async (id) => {
 const displayNews = datas => {
     console.log(datas);
     const newsSection = document.getElementById('news-section');
-
+    newsSection.innerHTML = '';
+    console.log(newsSection);
     datas.forEach(data => {
         console.log(data);
-
         const div = document.createElement('div');
         div.innerHTML = `
+        <div class="row p-2 border">
+                    <div class="col align-self-start">
+                        <div><img class="img-thumbnail" src="${data.image_url}" alt="Movie"></div>
+                    </div>
+                    <div class="col align-self-start">
+                        <div class="card-body">
+                            <h4 class="card-title">${data.title}</h4>
+                            <p>${data.details.slice(0, 300)}....</p>
 
+                            <div class="d-flex justify-content-between align-items-center">
+                                <!-- author -->
+                                <div class="d-flex">
+                                    <div>
+                                        <img class="rounded-circle" style="width:50px" src="${data.author.img}" />
+                                    </div>
+                                    <div class="">
+                                        <span class="fw-bold">${data.author.name}</span>
+                                        <p class="text-muted">${data.author.published_date}</p>
+                                    </div>
+                                </div>
+
+                                <!-- views -->
+                                <div>
+                                    <span><i class="fa-solid fa-eye"></i> 1.4 M</span>
+                                </div>
+                                <!-- Icons -->
+                                <div>
+                                    <i class="fa-solid fa-star text-orange-400"></i>
+                                    <i class="fa-solid fa-star text-orange-400"></i>
+                                    <i class="fa-solid fa-star text-orange-400"></i>
+                                    <i class="fa-solid fa-star text-orange-400"></i>
+                                    <i class="fa-solid fa-star text-orange-400"></i>
+                                </div>
+                                <!-- button -->
+                                <div class="">
+                                    <button class="btn btn-primary">Watch</button>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
 
         `
         newsSection.appendChild(div);
